e27d4e214eb984a677e701fe1c7b9b0b7df86377
Dusica <dusica.jokic@fourity.com>
2021-03-01 07:55:24 +0100

    Add Controller

---
+++ marvello-ws/Marvello/Marvello.WebApi/Controllers/CommentController.cs
@@ -0,0 +1,73 @@
+﻿using Marvello.Service.DTOs;
+using Marvello.Service.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Marvello.WebApi.Controllers
+{
+    [Route("api/comments")]
+    [ApiController]
+    public class CommentController : ControllerBase
+    {
+        private readonly ICommentService _commentService;
+        public CommentController(ICommentService commentService) 
+        {
+            _commentService = commentService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var listComment = await _commentService.GetAll();
+            return Ok(listComment);            
+        }
+
+        [HttpGet("id")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var oneComment = await _commentService.GetOne(id);
+            if(oneComment == null)
+            {
+                return NotFound();
+            }
+            return Ok(oneComment);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateComment([FromBody] CommentDTO entity)
+        {
+            if(entity == null)
+            {
+                return BadRequest();
+            }
+            var saveComment = await _commentService.Save(entity);
+            return Created("localhost:8080/api/comments/" + saveComment.Id, saveComment);
+        }  
+
+        [HttpPut("id")]
+        public async Task<IActionResult> UpdateComment([FromBody] CommentDTO entity, int id)
+        {
+            if(entity == null || id == 0)
+            {
+                return BadRequest();
+            }
+            var updateComment = await _commentService.Update(entity);
+            return Ok(updateComment);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteComment([FromBody] CommentDTO entity)
+        {
+            if(entity == null)
+            {
+                return BadRequest();
+            }
+            await _commentService.Delete(entity);
+            return Ok();
+        }
+    }
+}
---
+++ marvello-ws/Marvello/Marvello.WebApi/Controllers/ProjectController.cs
@@ -0,0 +1,73 @@
+﻿using Marvello.Service.DTOs;
+using Marvello.Service.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Marvello.WebApi.Controllers
+{
+    [Route("api/projects")]
+    [ApiController]
+    public class ProjectController : ControllerBase
+    {
+        private readonly IProjectService _projectService;
+        public ProjectController(IProjectService projectService)
+        {
+            _projectService = projectService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProject()
+        {
+            var listProject = await _projectService.GetAll();
+            return Ok(listProject);
+        }
+
+        [HttpGet("id")]
+        public async Task<IActionResult> GetOneProject(long id)
+        {
+            var oneProject = await _projectService.GetOne(id);
+            if(oneProject == null)
+            {
+                return NotFound();
+            }
+            return Ok(oneProject);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SaveProject([FromBody] ProjectDTO entity)
+        {
+            if(entity == null)
+            {
+                return BadRequest();
+            }
+            var saveProject = await _projectService.Save(entity);
+            return Created("localhost:8080/api/projects/" + saveProject.Id, saveProject);
+        }
+
+        [HttpPut("id")]
+        public async Task<IActionResult> UpdateProject([FromBody] ProjectDTO entity, long id)
+        {
+            if (entity == null || id == 0)
+            {
+                return BadRequest();
+            }
+            var updateProject = await _projectService.Update(entity);
+            return Ok(updateProject);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteProject([FromBody] ProjectDTO entity)
+        {
+           if(entity == null)
+            {
+                return BadRequest();
+            }
+            await _projectService.Delete(entity);
+            return Ok();
+        }
+    }
+}
